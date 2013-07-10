6d8a5ea0099cd04aa361d9b8e9f2858a30f3447c
Scott Goci <scottjg@gmail.com>
2013-07-10 17:14:06 -0400

    example app working with Rails assets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bigvideo::Application.config.secret_token = 'dfc3e035f93b9ea71ef00496afab8253c3a9c3fbf30c7f7a63f187d78d1c141e5741cebb8b7829e5bd4668606ca18fff981a879f6157f3a258744bd1dc36052d'
